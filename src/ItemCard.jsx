3359129531f4a5950f4a594c270cc9b09505d236
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-05-21 22:21:36 +0530

    Connected to Backend Finally ;)

--- src/ItemCard.jsx
+++ src/ItemCard.jsx
@@ -17,21 +17,23 @@ function ItemCard(props) {
         setLiked(!liked);
     }
 
+    const mileage = Math.round(Number((props.car_data.km/1000).toFixed(1))*10)/10;
+
     return(
         <div className="card" onClick={itemClicked}>
             <img className="like-btn" src={liked ? "red_heart.svg" : "gray_heart.svg"} style={{opacity: liked ? "75%" : "21%"}} onClick={likeCar}/>
             <img className="card-image" src={props.car_data.image} />
             <p className="car-model"><span style={{color:"#313131", fontSize:17.3}}>{props.car_data.year} {props.car_data.brand} {props.car_data.model}</span> <span style={{fontWeight:350}}>{props.car_data.variant}</span></p>
             <div className="info-card">
-                <p className="info">48.2k km</p>
-                <p className="info">Petrol</p>
-                <p className="info">Auto</p>
-                <p className="info">1st owner</p>
+                <p className="info">{mileage}K km</p>
+                <p className="info">{props.car_data.fuel}</p>
+                <p className="info">{props.car_data.transmission == "Automatic" ? "Auto" : "Manual"}</p>
+                <p className="info">{props.car_data.ownerCount} owner</p>
             </div>
             <div className="info-card" style={{justifyContent:"space-between", paddingRight:"15px"}}>
                 <p className="price-text">EMI ₹5,327/m</p>
                 <div>
-                <p className="price-text" style={{fontSize:"15.2px", fontFamily:"Poppins", fontWeight:500, borderBottom:0, marginBottom:0, paddingBottom:0}}>₹3.53 lakh</p>
+                <p className="price-text" style={{fontSize:"15.2px", fontFamily:"Poppins", fontWeight:500, borderBottom:0, marginBottom:0, paddingBottom:0}}>₹{props.car_data.price} lakh</p>
                 <p className="price-text" style={{fontSize:"12px", paddingBottom:"10px", marginTop:0, paddingBottom:2, color:"#7b7b7b", fontFamily:"Poppins", fontWeight:400}}>+ dealer charges</p>
                 </div>
             </div>
